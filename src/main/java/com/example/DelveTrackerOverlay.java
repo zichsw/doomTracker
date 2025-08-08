36ef080916ce3900bc19777212df227cade32403
Steve W <zichsw@gmail.com>
2025-08-08 19:17:06 -0400

    test

---
+++ src/main/java/com/example/DelveTrackerOverlay.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import net.runelite.api.Client;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+
+import javax.inject.Inject;
+import java.awt.*;
+import java.util.List;
+
+
+public class DelveTrackerOverlay extends OverlayPanel {
+    private DelveTrackerPlugin plugin;
+    private Client client;
+    private DelveTrackerConfig config;
+
+
+    @Inject
+    public DelveTrackerOverlay(DelveTrackerPlugin plugin, Client client, DelveTrackerConfig config) {
+        super(plugin);
+        this.client = client;
+        this.config = config;
+        setPosition(OverlayPosition.TOP_LEFT);
+    }
+
+    @Override
+    public Dimension render(Graphics2D graphics){
+        panelComponent.getChildren().clear();
+
+        if (!plugin.inRegion()){
+            return null;  //don't render if not in room
+        }
+
+        panelComponent.getChildren().add(LineComponent.builder()
+                .left("Boss HP:")
+                .right(String.valueOf(plugin.getLevels()))
+                .build());
+
+        return super.render(graphics);
+    }
+}
---
+++ src/main/java/com/example/DelveTrackerPlugin.java
@@ -0,0 +1,84 @@
+package com.example;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.ChatMessage;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+import java.util.Set;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Example"
+)
+public class DelveTrackerPlugin extends Plugin
+{
+	private static final Set<Integer> REGION_IDS = Set.of(5269, 13668, 14180);
+	private int levels = 0;
+
+	@Inject
+	private Client client;
+
+	@Inject
+	private DelveTrackerConfig config;
+
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private DelveTrackerOverlay overlay;
+
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		overlayManager.add(overlay);
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		overlayManager.remove(overlay);
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage chatMessage){
+		if (chatMessage.getType() == ChatMessageType.GAMEMESSAGE && inRegion()){
+			String message = chatMessage.getMessage();
+			if (message.contains("Completed!")){
+				addLevel();
+			}
+		}
+	}
+
+
+	public boolean inRegion(){
+		final var region = client.getTopLevelWorldView();
+		return REGION_IDS.contains(region.getId()) && region.isInstance();
+	}
+
+
+	public void addLevel(){
+		levels += 1;
+	}
+
+	public int getLevels() {
+		return levels;
+	}
+
+	@Provides
+	DelveTrackerConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(DelveTrackerConfig.class);
+	}
+
+}
